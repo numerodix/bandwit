89b096efabd76f86a1bd46d8b0cffedd4b8e5270
Martin Matusiak <numerodix@gmail.com>
2020-04-08 06:59:46 +1000

    move into cpp file

---
+++ src/termui/terminal_mode.cpp
@@ -0,0 +1,82 @@
+#include <stdexcept>
+#include <unistd.h>
+
+#include "signals.h"
+#include "terminal_mode.h"
+
+namespace bmon {
+namespace termui {
+
+void TerminalModeSetter::set() {
+    SignalGuard guard{signal_suspender_};
+
+    struct termios tm {};
+
+    if (tcgetattr(STDIN_FILENO, &tm) < 0) {
+        throw std::runtime_error(
+            "TerminalModeSetter.set failed in tcgetattr()");
+    }
+
+    // save the unmodified state so we can restore it
+    orig_termios_ = tm;
+
+    tm.c_lflag &= ~local_off_;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &tm) < 0) {
+        throw std::runtime_error(
+            "TerminalModeSetter.set failed in tcsetattr()");
+    }
+
+    // Now check that the set actually set all of our flags
+
+    struct termios tm_after {};
+    if (tcgetattr(STDIN_FILENO, &tm_after) < 0) {
+        throw std::runtime_error(
+            "TerminalModeSetter.set failed in #2 tcgetattr()");
+    }
+
+    if ((tm_after.c_lflag & local_off_) > 0) {
+        throw std::runtime_error(
+            "TerminalModeSetter.set failed to actually set the flags!");
+    }
+}
+
+void TerminalModeSetter::unset() {
+    SignalGuard guard{signal_suspender_};
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios_) < 0) {
+        throw std::runtime_error(
+            "TerminalModeSetter.unset failed in tcsetattr()");
+    }
+
+    // Now check that the set actually unset all of our flags
+
+    struct termios tm_after {};
+    if (tcgetattr(STDIN_FILENO, &tm_after) < 0) {
+        throw std::runtime_error(
+            "TerminalModeSetter.unset failed in tcgetattr()");
+    }
+
+    if ((tm_after.c_lflag & local_off_) != local_off_) {
+        throw std::runtime_error(
+            "TerminalModeSetter.unset failed to actually unset the flags!");
+    }
+}
+
+TerminalModeSet &TerminalModeSet::local_on(tcflag_t flag) {
+    flags_local_on_ |= flag;
+    return *this;
+}
+
+TerminalModeSet &TerminalModeSet::local_off(tcflag_t flag) {
+    flags_local_off_ |= flag;
+    return *this;
+}
+
+TerminalModeSetter TerminalModeSet::build_setter(SignalSuspender *signal_suspender) {
+    TerminalModeSetter setter{flags_local_off_, signal_suspender};
+    return setter;
+}
+
+} // namespace termui
+} // namespace bmon
--- src/termui/terminal_mode.h
+++ src/termui/terminal_mode.h
@@ -1,17 +1,12 @@
 #ifndef TERMINAL_MODE_H
 #define TERMINAL_MODE_H
 
-#include <initializer_list>
-#include <iostream>
-#include <stdexcept>
 #include <termios.h>
-#include <unistd.h>
-#include <vector>
-
-#include "signals.h"
 
 namespace bmon {
 namespace termui {
+
+class SignalSuspender;
 
 class TerminalModeSetter {
   public:
@@ -19,61 +14,8 @@ class TerminalModeSetter {
                                 SignalSuspender *signal_suspender)
         : local_off_{local_off}, signal_suspender_{signal_suspender} {}
 
-    void set() {
-        SignalGuard guard{signal_suspender_};
-
-        struct termios tm {};
-
-        if (tcgetattr(STDIN_FILENO, &tm) < 0) {
-            throw std::runtime_error(
-                "TerminalModeSetter.set failed in tcgetattr()");
-        }
-
-        // save the unmodified state so we can restore it
-        orig_termios_ = tm;
-
-        tm.c_lflag &= ~local_off_;
-
-        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &tm) < 0) {
-            throw std::runtime_error(
-                "TerminalModeSetter.set failed in tcsetattr()");
-        }
-
-        // Now check that the set actually set all of our flags
-
-        struct termios tm_after {};
-        if (tcgetattr(STDIN_FILENO, &tm_after) < 0) {
-            throw std::runtime_error(
-                "TerminalModeSetter.set failed in #2 tcgetattr()");
-        }
-
-        if ((tm_after.c_lflag & local_off_) > 0) {
-            throw std::runtime_error(
-                "TerminalModeSetter.set failed to actually set the flags!");
-        }
-    }
-
-    void unset() {
-        SignalGuard guard{signal_suspender_};
-
-        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios_) < 0) {
-            throw std::runtime_error(
-                "TerminalModeSetter.unset failed in tcsetattr()");
-        }
-
-        // Now check that the set actually unset all of our flags
-
-        struct termios tm_after {};
-        if (tcgetattr(STDIN_FILENO, &tm_after) < 0) {
-            throw std::runtime_error(
-                "TerminalModeSetter.unset failed in tcgetattr()");
-        }
-
-        if ((tm_after.c_lflag & local_off_) != local_off_) {
-            throw std::runtime_error(
-                "TerminalModeSetter.unset failed to actually unset the flags!");
-        }
-    }
+    void set();
+    void unset();
 
   private:
     tcflag_t local_off_{};
@@ -84,20 +26,9 @@ class TerminalModeSetter {
 
 class TerminalModeSet {
   public:
-    TerminalModeSet &local_on(tcflag_t flag) {
-        flags_local_on_ |= flag;
-        return *this;
-    }
-
-    TerminalModeSet &local_off(tcflag_t flag) {
-        flags_local_off_ |= flag;
-        return *this;
-    }
-
-    TerminalModeSetter build_setter(SignalSuspender *signal_suspender) {
-        TerminalModeSetter setter{flags_local_off_, signal_suspender};
-        return setter;
-    }
+    TerminalModeSet &local_on(tcflag_t flag);
+    TerminalModeSet &local_off(tcflag_t flag);
+    TerminalModeSetter build_setter(SignalSuspender *signal_suspender);
 
   private:
     tcflag_t flags_local_on_{};
