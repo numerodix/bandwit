19787fe0f2a77ef4960de11e89ce4b59c0a92057
Martin Matusiak <numerodix@gmail.com>
2020-04-07 16:08:11 +1000

    use arguments

--- src/termui/dummy.h
+++ src/termui/dummy.h
@@ -167,8 +167,9 @@ class TerminalDriver {
 
     Dimensions get_terminal_size() {
         struct winsize size {};
+        int stdout_fileno = fileno(stdout_file_);
 
-        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &size) < 0) {
+        if (ioctl(stdout_fileno, TIOCGWINSZ, &size) < 0) {
             throw std::runtime_error(
                 "TerminalDriver.get_terminal_size failed in ioctl()");
         }
@@ -181,10 +182,10 @@ class TerminalDriver {
         // This is bit error prone: store the cursor position in the program to
         // avoid reading it more than just at the very beginning. Maybe on
         // startup just fall back on taking over the whole screen (or retry?).
-        fprintf(stdout, "\033[6n");
+        fprintf(stdout_file_, "\033[6n");
 
         int cur_x, cur_y;
-        if (fscanf(stdin, "\033[%d;%dR", &cur_y, &cur_x) < 2) {
+        if (fscanf(stdin_file_, "\033[%d;%dR", &cur_y, &cur_x) < 2) {
             throw std::runtime_error(
                 "TerminalDriver.get_cursor_position failed in scanf()");
         }
@@ -194,15 +195,15 @@ class TerminalDriver {
     }
 
     void set_cursor_position(const Point &pt) {
-        fprintf(stdout, "\033[%d;%dH", pt.y, pt.x);
+        fprintf(stdout_file_, "\033[%d;%dH", pt.y, pt.x);
     }
 
     void put_char(char ch) {
         char_str_[0] = ch;
-        fprintf(stdout, "%s", char_str_);
+        fprintf(stdout_file_, "%s", char_str_);
     }
 
-    void flush_output() { fflush(stdout); }
+    void flush_output() { fflush(stdout_file_); }
 
   private:
     FILE *stdin_file_{};
