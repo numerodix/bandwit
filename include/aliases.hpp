2291da0be2566b9f73c348a4369f8f0113aee037
Martin Matusiak <numerodix@gmail.com>
2020-04-23 17:35:31 +1000

    put aliases into namespace

--- include/aliases.hpp
+++ include/aliases.hpp
@@ -3,8 +3,12 @@
 
 #include <chrono>
 
+namespace bmon {
+
 using Clock = std::chrono::system_clock;
 using TimePoint = Clock::time_point;
 using Ms = std::chrono::milliseconds;
+
+} // namespace bmon
 
 #endif // ALIASES_H
