e4178844e5067e44c0aee7367abff5f982f4c3c9
Martin Matusiak <numerodix@gmail.com>
2020-04-25 15:13:37 +1000

    house keeping

--- src/sampling/sysfs_sampler.cpp
+++ src/sampling/sysfs_sampler.cpp
@@ -4,59 +4,43 @@
 #include <stdexcept>
 #include <unordered_map>
 
+#include "aliases.hpp"
 #include "sysfs_sampler.hpp"
 
 namespace bandwit {
 namespace sampling {
 
-class SysFsParser {
-
-  public:
-    enum class Quantity {
-        RX_BYTES,
-        TX_BYTES,
-    };
-
-    uint64_t read_file_as_number(const std::string &filepath) const {
-        std::ifstream fl{filepath};
-        if (!fl) {
-            throw std::runtime_error("failed to open file for reading");
-        }
-
-        std::string contents{};
-        getline(fl, contents);
-
-        return std::stoul(contents);
+uint64_t SysFsParser::read_file_as_number(const std::string &filepath) const {
+    std::ifstream fl{filepath};
+    if (!fl) {
+        throw std::runtime_error("failed to open file for reading");
     }
 
-    std::string create_filepath(const std::string &iface_name,
-                                const Quantity &qtty) const {
-        std::stringstream ss{};
-        std::string filename = quantity_filenames_.at(qtty);
-        ss << "/sys/class/net/" << iface_name << "/statistics/" << filename;
-        return ss.str();
-    }
+    std::string contents{};
+    getline(fl, contents);
 
-    uint64_t read(const std::string &iface_name, const Quantity &qtty) const {
-        std::string filepath = create_filepath(iface_name, qtty);
-        return read_file_as_number(filepath);
-    }
+    return std::stoul(contents);
+}
 
-  private:
-    std::unordered_map<Quantity, std::string> quantity_filenames_{
-        {Quantity::RX_BYTES, "rx_bytes"},
-        {Quantity::TX_BYTES, "tx_bytes"},
-    };
-};
+std::string SysFsParser::create_filepath(const std::string &iface_name,
+                                         const Quantity &qtty) const {
+    std::stringstream ss{};
+    std::string filename = quantity_filenames_.at(qtty);
+    ss << "/sys/class/net/" << iface_name << "/statistics/" << filename;
+    return ss.str();
+}
+
+uint64_t SysFsParser::read(const std::string &iface_name, const Quantity &qtty) const {
+    std::string filepath = create_filepath(iface_name, qtty);
+    return read_file_as_number(filepath);
+}
 
 Sample SysFsSampler::get_sample(const std::string &iface_name) const {
-    SysFsParser parser{};
+    auto tp = Clock::now();
+    std::time_t ts = Clock::to_time_t(tp);
 
-    auto tp = std::chrono::system_clock::now();
-    std::time_t ts = std::chrono::system_clock::to_time_t(tp);
-
-    uint64_t rx = parser.read(iface_name, SysFsParser::Quantity::RX_BYTES);
-    uint64_t tx = parser.read(iface_name, SysFsParser::Quantity::TX_BYTES);
+    uint64_t rx = parser_.read(iface_name, SysFsParser::Quantity::RX_BYTES);
+    uint64_t tx = parser_.read(iface_name, SysFsParser::Quantity::TX_BYTES);
 
     Sample sample{
         rx,
--- src/sampling/sysfs_sampler.hpp
+++ src/sampling/sysfs_sampler.hpp
@@ -1,13 +1,33 @@
 #ifndef SYSFS_SAMPLER_H
 #define SYSFS_SAMPLER_H
 
+#include <unordered_map>
+
 #include "sampling/sampler.hpp"
 
 namespace bandwit {
 namespace sampling {
 
-class SysFsSampler : public Sampler {
+class SysFsParser {
+  public:
+    enum class Quantity {
+        RX_BYTES,
+        TX_BYTES,
+    };
 
+    uint64_t read_file_as_number(const std::string &filepath) const;
+    std::string create_filepath(const std::string &iface_name,
+                                const Quantity &qtty) const;
+    uint64_t read(const std::string &iface_name, const Quantity &qtty) const;
+
+  private:
+    std::unordered_map<Quantity, std::string> quantity_filenames_{
+        {Quantity::RX_BYTES, "rx_bytes"},
+        {Quantity::TX_BYTES, "tx_bytes"},
+    };
+};
+
+class SysFsSampler : public Sampler {
   public:
     SysFsSampler() = default;
     ~SysFsSampler() override = default;
@@ -16,6 +36,9 @@ class SysFsSampler : public Sampler {
     CLASS_DISABLE_MOVES(SysFsSampler)
 
     Sample get_sample(const std::string &iface_name) const override;
+
+  private:
+    SysFsParser parser_{};
 };
 
 } // namespace sampling
