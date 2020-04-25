828df40cd494ff3b5dbe5e1e247a7f5ea107f94a
Martin Matusiak <numerodix@gmail.com>
2020-04-25 22:01:16 +1000

    update tidy rules

--- cmake/static_analyzers.cmake
+++ cmake/static_analyzers.cmake
@@ -1,7 +1,5 @@
 option(ENABLE_CLANG_TIDY "Enable clang-tidy during compilation" OFF)
 
 if(ENABLE_CLANG_TIDY)
-    set(CMAKE_CXX_CLANG_TIDY clang-tidy;
-        -header-filter=.;
-        -checks=*,-cert-env33-c,-fuchsia-*,-llvm-header-guard,-misc-macro-parentheses;)
+    set(CMAKE_CXX_CLANG_TIDY clang-tidy)
 endif()
