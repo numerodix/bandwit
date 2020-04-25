e7f48f2a31a3681a4d81a4b7c4858ccef6605bc4
Martin Matusiak <numerodix@gmail.com>
2020-04-25 15:18:52 +1000

    house keeping

--- src/sampling/procfs_sampler.cpp
+++ src/sampling/procfs_sampler.cpp
@@ -4,67 +4,59 @@
 #include <stdexcept>
 #include <vector>
 
+#include "aliases.hpp"
 #include "procfs_sampler.hpp"
 
 namespace bandwit {
 namespace sampling {
 
-class ProcFsParser {
-  public:
-    std::vector<std::string> read_file_as_lines() const {
-        std::ifstream fl{filepath_};
-        if (!fl) {
-            throw std::runtime_error("failed to open file for reading");
-        }
-
-        std::string line{};
-        std::vector<std::string> lines{};
-
-        while (getline(fl, line)) {
-            lines.push_back(line);
-        }
-
-        return lines;
+std::vector<std::string> ProcFsParser::read_file_as_lines() const {
+    std::ifstream fl{filepath_};
+    if (!fl) {
+        throw std::runtime_error("failed to open file for reading");
     }
 
-    std::pair<uint64_t, uint64_t> parse(const std::vector<std::string> &lines,
-                                        const std::string &iface_name) const {
-        for (const std::string &line : lines) {
-            std::smatch mres;
+    std::string line{};
+    std::vector<std::string> lines{};
 
-            bool matches = std::regex_search(line, mres, pat_line_);
-            if (matches) {
-                std::string cur_iface = mres[1];
-                if (cur_iface == iface_name) {
-                    std::string rx_s = mres[2];
-                    std::string tx_s = mres[10];
+    while (getline(fl, line)) {
+        lines.push_back(line);
+    }
 
-                    uint64_t rx = std::stoul(rx_s);
-                    uint64_t tx = std::stoul(tx_s);
+    return lines;
+}
 
-                    return std::make_pair(rx, tx);
-                }
+std::pair<uint64_t, uint64_t>
+ProcFsParser::parse(const std::vector<std::string> &lines,
+                    const std::string &iface_name) const {
+    for (const std::string &line : lines) {
+        std::smatch mres;
+
+        bool matches = std::regex_search(line, mres, pat_line_);
+        if (matches) {
+            std::string cur_iface = mres[1];
+            if (cur_iface == iface_name) {
+                std::string rx_s = mres[2];
+                std::string tx_s = mres[10];
+
+                uint64_t rx = std::stoul(rx_s);
+                uint64_t tx = std::stoul(tx_s);
+
+                return std::make_pair(rx, tx);
             }
         }
-
-        throw std::runtime_error(
-            "failed to find the right iface / parse output");
     }
 
-  private:
-    std::string filepath_{"/proc/net/dev"};
-    std::regex pat_line_{
-        R"(^\s*([A-Za-z0-9]+):\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+))"};
-};
+    throw std::runtime_error(
+        "failed to find the right iface / parse output");
+}
 
 Sample ProcFsSampler::get_sample(const std::string &iface_name) const {
-    ProcFsParser parser{};
+    auto tp = Clock::now();
+    std::time_t ts = Clock::to_time_t(tp);
 
-    auto tp = std::chrono::system_clock::now();
-    std::time_t ts = std::chrono::system_clock::to_time_t(tp);
-
-    auto lines = parser.read_file_as_lines();
-    auto pair = parser.parse(lines, iface_name);
+    auto lines = parser_.read_file_as_lines();
+    auto pair = parser_.parse(lines, iface_name);
 
     Sample sample{
         pair.first,
--- src/sampling/procfs_sampler.hpp
+++ src/sampling/procfs_sampler.hpp
@@ -1,13 +1,27 @@
 #ifndef PROCFS_SAMPLER_H
 #define PROCFS_SAMPLER_H
 
+#include <regex>
+#include <string>
+
 #include "sampling/sampler.hpp"
 
 namespace bandwit {
 namespace sampling {
 
-class ProcFsSampler : public Sampler {
+class ProcFsParser {
+  public:
+    std::vector<std::string> read_file_as_lines() const;
+    std::pair<uint64_t, uint64_t> parse(const std::vector<std::string> &lines,
+                                        const std::string &iface_name) const;
 
+  private:
+    std::string filepath_{"/proc/net/dev"};
+    std::regex pat_line_{
+        R"(^\s*([A-Za-z0-9]+):\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+))"};
+};
+
+class ProcFsSampler : public Sampler {
   public:
     ProcFsSampler() = default;
     ~ProcFsSampler() override = default;
@@ -16,6 +30,9 @@ class ProcFsSampler : public Sampler {
     CLASS_DISABLE_MOVES(ProcFsSampler)
 
     Sample get_sample(const std::string &iface_name) const override;
+
+  private:
+    ProcFsParser parser_{};
 };
 
 } // namespace sampling
