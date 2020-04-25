9b3c8ba38ebd6569132c3b8c86cf26404b7782cd
Martin Matusiak <numerodix@gmail.com>
2020-04-25 15:06:48 +1000

    house keeping

--- src/sampling/ip_cmd_sampler.cpp
+++ src/sampling/ip_cmd_sampler.cpp
@@ -2,108 +2,96 @@
 #include <chrono>
 #include <iostream>
 #include <memory>
-#include <regex>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 
+#include "aliases.hpp"
 #include "ip_cmd_sampler.hpp"
-#include "program_runner.hpp"
 
 namespace bandwit {
 namespace sampling {
 
-class IpStatsParser {
-  public:
-    uint64_t parse_nbytes(const std::string &line) {
-        std::smatch mres{};
-        bool matches = std::regex_search(line, mres, pat_bytes_);
-        if (!matches) {
-            throw std::runtime_error("regex pat_bytes_ failed to match line");
-        }
-
-        std::string bytes_s = mres[1];
-        return std::stoul(bytes_s);
+uint64_t IpStatsParser::parse_nbytes(const std::string &line) const {
+    std::smatch mres{};
+    bool matches = std::regex_search(line, mres, pat_bytes_);
+    if (!matches) {
+        throw std::runtime_error("regex pat_bytes_ failed to match line");
     }
 
-    std::pair<uint64_t, uint64_t> parse(const std::vector<std::string> &lines,
-                                        const std::string &iface_name) {
-        std::string cur_iface{};
+    std::string bytes_s = mres[1];
+    return std::stoul(bytes_s);
+}
 
-        bool next_line_is_rx{false};
-        bool next_line_is_tx{false};
+std::pair<uint64_t, uint64_t>
+IpStatsParser::parse(const std::vector<std::string> &lines,
+                     const std::string &iface_name) const {
+    std::string cur_iface{};
 
-        int64_t rx{-1};
-        int64_t tx{-1};
+    bool next_line_is_rx{false};
+    bool next_line_is_tx{false};
 
-        for (const std::string &line : lines) {
-            // Did we match RX: on the previous line?
-            if (next_line_is_rx) {
-                rx = parse_nbytes(line);
-                next_line_is_rx = false;
-            }
+    int64_t rx{-1};
+    int64_t tx{-1};
 
-            // Did we match TX: on the previous line?
-            if (next_line_is_tx) {
-                tx = parse_nbytes(line);
-                next_line_is_tx = false;
-            }
-
-            // match iface name
-            std::smatch mres_iface;
-            bool matches = std::regex_search(line, mres_iface, pat_iface_);
-            if (matches) {
-                cur_iface = mres_iface[2];
-                continue;
-            }
-
-            // match RX: line
-            matches = std::regex_search(line, pat_rx_);
-            if (matches) {
-                next_line_is_rx = true;
-                continue;
-            }
-
-            // match TX: line
-            matches = std::regex_search(line, pat_tx_);
-            if (matches) {
-                next_line_is_tx = true;
-                continue;
-            }
-
-            // We've found the right iface and we've parsed rx and tx!
-            if ((cur_iface == iface_name) && (rx >= 0 && tx >= 0)) {
-                auto urx = U64(rx);
-                auto utx = U64(tx);
-                return std::make_pair(urx, utx);
-            }
+    for (const std::string &line : lines) {
+        // Did we match RX: on the previous line?
+        if (next_line_is_rx) {
+            rx = parse_nbytes(line);
+            next_line_is_rx = false;
         }
 
-        throw std::runtime_error(
-            "failed to find the right iface / parse output");
+        // Did we match TX: on the previous line?
+        if (next_line_is_tx) {
+            tx = parse_nbytes(line);
+            next_line_is_tx = false;
+        }
+
+        // match iface name
+        std::smatch mres_iface;
+        bool matches = std::regex_search(line, mres_iface, pat_iface_);
+        if (matches) {
+            cur_iface = mres_iface[2];
+            continue;
+        }
+
+        // match RX: line
+        matches = std::regex_search(line, pat_rx_);
+        if (matches) {
+            next_line_is_rx = true;
+            continue;
+        }
+
+        // match TX: line
+        matches = std::regex_search(line, pat_tx_);
+        if (matches) {
+            next_line_is_tx = true;
+            continue;
+        }
+
+        // We've found the right iface and we've parsed rx and tx!
+        if ((cur_iface == iface_name) && (rx >= 0 && tx >= 0)) {
+            auto urx = U64(rx);
+            auto utx = U64(tx);
+            return std::make_pair(urx, utx);
+        }
     }
 
-  private:
-    std::regex pat_iface_{R"(^([0-9]+): ([A-Za-z0-9]+):)"};
-    std::regex pat_rx_{R"(^    RX)"};
-    std::regex pat_tx_{R"(^    TX)"};
-    std::regex pat_bytes_{R"(^    ([0-9]+))"};
-};
+    throw std::runtime_error(
+        "failed to find the right iface / parse output");
+}
 
 Sample IpCommandSampler::get_sample(const std::string &iface_name) const {
-    ProgramRunner runner{};
-    IpStatsParser parser{};
-
-    auto tp = std::chrono::system_clock::now();
-    std::time_t ts = std::chrono::system_clock::to_time_t(tp);
+    auto tp = Clock::now();
+    std::time_t ts = Clock::to_time_t(tp);
 
     std::stringstream ss{};
     ss << "ip -statistics link show dev " << iface_name;
     std::string args = ss.str();
 
-    auto output = runner.run(args);
-    auto pair = parser.parse(output, iface_name);
+    auto output = runner_.run(args);
+    auto pair = parser_.parse(output, iface_name);
 
     Sample sample{
         pair.first,
--- src/sampling/ip_cmd_sampler.hpp
+++ src/sampling/ip_cmd_sampler.hpp
@@ -1,13 +1,28 @@
 #ifndef IP_CMD_SAMPLER_H
 #define IP_CMD_SAMPLER_H
 
+#include <regex>
+
+#include "sampling/program_runner.hpp"
 #include "sampling/sampler.hpp"
 
 namespace bandwit {
 namespace sampling {
 
-class IpCommandSampler : public Sampler {
+class IpStatsParser {
+  public:
+    uint64_t parse_nbytes(const std::string &line) const;
+    std::pair<uint64_t, uint64_t> parse(const std::vector<std::string> &lines,
+                                        const std::string &iface_name) const;
 
+  private:
+    std::regex pat_iface_{R"(^([0-9]+): ([A-Za-z0-9]+):)"};
+    std::regex pat_rx_{R"(^    RX)"};
+    std::regex pat_tx_{R"(^    TX)"};
+    std::regex pat_bytes_{R"(^    ([0-9]+))"};
+};
+
+class IpCommandSampler : public Sampler {
   public:
     IpCommandSampler() = default;
     ~IpCommandSampler() override = default;
@@ -16,6 +31,10 @@ class IpCommandSampler : public Sampler {
     CLASS_DISABLE_MOVES(IpCommandSampler)
 
     Sample get_sample(const std::string &iface_name) const override;
+
+  private:
+    ProgramRunner runner_{};
+    IpStatsParser parser_{};
 };
 
 } // namespace sampling
